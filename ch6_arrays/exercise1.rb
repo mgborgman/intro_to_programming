e4226a554996188e0c3e00c5766b5e389646b8f6
Matt Borgman <mgborgman@gmail.com>
2016-09-11 17:49:00 -0700

    re-working exercises

---
+++ ch6_arrays/exercise1.rb
@@ -0,0 +1,8 @@
+arr = [1, 3, 5, 7, 9, 11]
+number = 3
+
+if arr.include?(3)
+	puts "We have a match!"
+else
+	puts "That number is no where to be found."
+end
---
+++ ch6_arrays/exercise7.rb
@@ -0,0 +1,6 @@
+original_array = [1, 2, 3, 4, 5]
+
+new_array = original_array.map { |item| item + 2 }
+
+p "Original Array: #{original_array}"
+p "New Array: #{new_array}"
